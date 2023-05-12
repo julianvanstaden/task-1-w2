74dfd87b9d112feedfbe4055bd502617ec7acb1b
julianvanstaden <125472051+julianvanstaden@users.noreply.github.com>
2023-05-12 07:06:02 +0200

    Data Type Exploration

---
+++ js/index.js
@@ -0,0 +1,64 @@
+let title = "Exercise One"
+let firstName = "Julian "
+let surname = "Van Staden"
+let age = 19
+const subject = "Development"
+
+let message = "Hello my name is " + firstName + ". I am " + age + " years old and I am studying " + subject + "."
+
+console.log(title)
+console.log(message)
+
+let space = " "
+console.log(space)
+
+let titleTwo = "Exercise Two"
+let numOne = -9
+let numTwo = 45 
+let numThree = 15
+let numFour = 3
+
+let answer = numOne + numTwo / numThree * numFour
+
+console.log(titleTwo)
+console.log(answer)
+
+space = " "
+console.log(space)
+
+
+let titleThree = "Exercise Three"
+let timmy = 10
+let susan = 25
+
+let timmyTax = 15/100 * 10
+let susanTax = 26/100 * 25
+
+let value = timmyTax + susanTax
+
+console.log(titleThree)
+console.log(value)
+
+space = " "
+console.log(space)
+
+
+let titleFour = "Exercise Four"
+let john = 23
+let tim = 67
+let alfie = 98
+let gwen = 45
+
+let total = john + tim + alfie + gwen
+let average = total/400 * 100
+
+let messageThree = "The students average is " + average
+
+let extra = 233 + 28
+
+average = extra/400 * 100
+messageTwo = "The students updated average is " + average
+
+console.log(titleFour)
+console.log(messageThree)
+console.log(messageTwo)
